4889fc14fb47d66f0770d3dd9de13817b573ccfc
Shayne Hartford <shaybox@shaybox.com>
2023-11-26 17:43:53 -0500

    0.1.1 - Add Temperature

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 use anyhow::{Error, Result};
 use derivative::Derivative;
 use register::Register;
-use serialize::{Information, Initialization};
+use serialize::{HighLowPair, Information, Initialization, SerialNumber};
 use tokio_modbus::{client::Context, prelude::*, Address, Quantity};
 use tokio_serial::SerialStream;
 
@@ -28,7 +28,6 @@ impl Ruiden {
     /* Constructors */
 
     /// # Errors
-    ///
     /// Will return `Err` if `SerialStream::open` errors
     pub fn new<'a>(
         path: impl Into<std::borrow::Cow<'a, str>>,
@@ -50,7 +49,16 @@ impl Ruiden {
     /* Wrappers */
 
     /// # Errors
-    ///
+    /// Will return `Err` if `Context::read_multiple` errors
+    pub async fn read_high_low(&mut self, register: Register) -> Result<u16> {
+        let words = self.read_multiple(register as Address, 2).await?;
+        let word_pair = (words[0], words[1]);
+        let high_low_pair = Into::<HighLowPair>::into(word_pair);
+
+        Ok(high_low_pair.0)
+    }
+
+    /// # Errors
     /// Will return `Err` if `Context::read_holding_registers` errors
     pub async fn read_multiple(&mut self, address: Address, quantity: Quantity) -> Result<Words> {
         self.ctx
@@ -60,14 +68,12 @@ impl Ruiden {
     }
 
     /// # Errors
-    ///
     /// Will return `Err` if `Self::read` errors
     pub async fn read_one(&mut self, register: Register) -> Result<Word> {
         Ok(self.read_multiple(register as Address, 1).await?[0])
     }
 
     /// # Errors
-    ///
     /// Will return `Err` if `Context::write_multiple_registers` errors
     pub async fn write_multiple(&mut self, register: Register, words: Words) -> Result<()> {
         self.ctx
@@ -77,7 +83,6 @@ impl Ruiden {
     }
 
     /// # Errors
-    ///
     /// Will return `Err` if `Context::write_single_register` errors
     pub async fn write_one(&mut self, register: Register, word: Word) -> Result<()> {
         self.ctx
@@ -89,7 +94,6 @@ impl Ruiden {
     /* Fetchers */
 
     /// # Errors
-    ///
     /// Will return `Err` if `Self::read_multiple` errors
     pub async fn fetch_all(&mut self) -> Result<()> {
         let address = Register::ID as Address;
@@ -102,7 +106,6 @@ impl Ruiden {
     }
 
     /// # Errors
-    ///
     /// Will return `Err` if `Self::read_multiple` errors
     pub async fn fetch_init(&mut self) -> Result<&Initialization> {
         let address = Register::ID as Address;
@@ -114,7 +117,6 @@ impl Ruiden {
     }
 
     /// # Errors
-    ///
     /// Will return `Err` if `Self::read_multiple` errors
     pub async fn fetch_info(&mut self) -> Result<&Information> {
         let address = Register::INT_C_S as Address;
@@ -128,11 +130,67 @@ impl Ruiden {
     /* Getters */
 
     /// # Errors
-    ///
     /// Will return `Err` if `Self::read_one` errors
     pub async fn get_id(&mut self) -> Result<u16> {
         self.read_one(Register::ID).await
     }
+
+    /// # Errors
+    /// Will return `Err` if `Self::read_multiple` errors
+    pub async fn get_sn(&mut self) -> Result<String> {
+        let high_low_pair = self.read_high_low(Register::SN_H).await?;
+        let serial_number = Into::<SerialNumber>::into(high_low_pair);
+
+        Ok(serial_number.0)
+    }
+
+    /// # Errors
+    /// Will return `Err` if `Self::read_one` errors
+    pub async fn get_fw(&mut self) -> Result<u16> {
+        self.read_one(Register::FW).await
+    }
+
+    /// # Errors
+    /// Will return `Err` if `Self::get_high_low_pair` errors
+    pub async fn get_int_c(&mut self) -> Result<u16> {
+        self.read_high_low(Register::INT_C_S).await
+    }
+
+    /// # Errors
+    /// Will return `Err` if `Self::get_high_low_pair` errors
+    pub async fn get_int_f(&mut self) -> Result<u16> {
+        self.read_high_low(Register::INT_F_S).await
+    }
+
+    // TODO: V_SET
+    // TODO: I_SET
+    // TODO: V_OUT
+    // TODO: I_OUT
+    // TODO: AH
+    // TODO: P_OUT
+    // TODO: V_IN
+    // TODO: KEYPAD
+    // TODO: OVP_OCP
+    // TODO: OUTPUT
+    // TODO: PRESET
+    // TODO: I_RANGE
+    // TODO: BAT_MODE
+    // TODO: V_BAT
+
+    /// # Errors
+    /// Will return `Err` if `Self::read_multiple` errors
+    pub async fn get_ext_c(&mut self) -> Result<u16> {
+        self.read_high_low(Register::EXT_C_S).await
+    }
+
+    /// # Errors
+    /// Will return `Err` if `Self::read_multiple` errors
+    pub async fn get_ext_f(&mut self) -> Result<u16> {
+        self.read_high_low(Register::EXT_F_S).await
+    }
+
+    // TODO: AH
+    // TODO: WH
 
     /* Setters */
 
--- src/serialize.rs
+++ src/serialize.rs
@@ -8,9 +8,14 @@ pub struct Initialization {
 }
 
 #[derive(Debug, Default)]
-pub struct Information {}
+pub struct Information {
+    pub int_c: u16,
+    pub int_f: u16,
+}
 
-struct SerialNumber(String);
+pub struct HighLowPair(pub u16);
+
+pub struct SerialNumber(pub String);
 
 impl From<Words> for Initialization {
     fn from(words: Words) -> Self {
@@ -23,13 +28,29 @@ impl From<Words> for Initialization {
 }
 
 impl From<Words> for Information {
-    fn from(_words: Words) -> Self {
-        Self {}
+    fn from(words: Words) -> Self {
+        Self {
+            int_c: Into::<HighLowPair>::into((words[0], words[1])).0,
+            int_f: Into::<HighLowPair>::into((words[2], words[3])).0,
+        }
+    }
+}
+
+impl From<WordPair> for HighLowPair {
+    fn from((high, low): WordPair) -> Self {
+        Self(high.wrapping_shl(16) | low)
+    }
+}
+
+impl From<u16> for SerialNumber {
+    fn from(high_low_pair: u16) -> Self {
+        Self(format!("{high_low_pair:08}"))
     }
 }
 
 impl From<WordPair> for SerialNumber {
-    fn from((high, low): WordPair) -> Self {
-        Self(format!("{:08}", high.wrapping_shl(16) | low))
+    fn from(word_pair: WordPair) -> Self {
+        let high_low_pair = Into::<HighLowPair>::into(word_pair);
+        Into::<Self>::into(high_low_pair.0)
     }
 }
